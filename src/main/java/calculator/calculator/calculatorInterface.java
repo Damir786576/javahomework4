7c7c27a2ee105db6b454c3a9a56b8d7ad6a45313
Student <andreevdamir266@gmail.com>
2024-12-02 15:31:26 +0300

    commit1

---
+++ src/main/java/calculator/calculator/calculatorInterface.java
@@ -0,0 +1,9 @@
+package calculator.calculator;
+
+public interface calculatorInterface {
+    String firstCalculator();
+    int plusCalculator(int plusNum1, int plusNum2);
+    int minusCalculator(int minusNum1, int minusNum2);
+    int multiplyCalculator(int multiplyNum1, int multiplyNum2);
+    String divideCalculator(int divideNum1, int divideNum2);
+}
---
+++ src/main/java/calculator/calculator/calculatorWork.java
@@ -0,0 +1,32 @@
+package calculator.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class calculatorWork implements calculatorInterface {
+    public String firstCalculator() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public int plusCalculator(int plusNum1, int plusNum2) {
+        return plusNum1 + plusNum2;
+    }
+
+    public int minusCalculator(int minusNum1, int minusNum2) {
+        return minusNum1 - minusNum1;
+    }
+
+    public int multiplyCalculator(int multiplyNum1, int multiplyNum2) {
+        return multiplyNum1 * multiplyNum2;
+    }
+
+    public String divideCalculator(int divideNum1, int divideNum2) {
+        if (divideNum2 == 0) {
+            return "Ошибка: Деление на ноль невозможно.";
+        }
+        return String.valueOf(divideNum1 / divideNum2);
+    }
+
+
+
+}
